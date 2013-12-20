56d5ccf828329236db8276c04ef2fcc142719e43
Philip Rowlands <flippons@gmail.com>
2013-12-20 13:31:44 +0100

    Uploaded new code files

---
+++ capturePoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// this class just tells the soldiers they have reached their destination
+
+public class capturePoint : MonoBehaviour {
+	
+	int objectLayer, blueLayer, orangeLayer;
+
+	// Use this for initialization
+	void Start () {
+	
+		blueLayer = LayerMask.NameToLayer("Blue");
+		orangeLayer = LayerMask.NameToLayer("Orange");
+	}
+	
+	// if a blue or orange unit enters the area, change their state
+	void OnTriggerEnter(Collider collidee)
+	{
+//		Debug.Log(collidee);
+		objectLayer = collidee.gameObject.layer;
+		
+		if (objectLayer == blueLayer || objectLayer == orangeLayer)
+		{
+			collidee.SendMessage("setState", "inPosition", SendMessageOptions.RequireReceiver);
+		}
+	}
+}
